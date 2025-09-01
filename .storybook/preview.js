15bfac68ecaf2bd10937f0ff020428267217acfc
Justin Lake <lake.justin.m@gmail.com>
2025-09-01 10:48:56 -0500

    feat: Intial Storybook setup configuration

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+const preview = {
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: 'Dark', value: '#121113' },
+        { name: 'Light', value: '#fff' },
+      ],
+      default: 'Light',
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default preview;
