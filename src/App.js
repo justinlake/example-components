17e3a7581bfa2f5079118f89d69fbe087ba6589a
Justin Lake <lake.justin.m@gmail.com>
2025-09-01 11:19:13 -0500

    feat: Add the generic project App and index files
    - These files are basic and suggest viewing Storybook instead

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <h1>Example Components</h1>
+            <p>More details regarding the components of this project can be seen by visiting the <a href="http://localhost:6006">Storybook instance</a>.</p>
+            <p>To get started, run the following command:</p>
+            <code>npm run storybook</code>
+        </React.Fragment>
+    );
+};
+
+export default App;
